cb43b781637091c87eac6c5e9357c75f5bbf69bb
BillyTheArtist <billy.iv.art@gmail.com>
2023-11-06 14:16:34 -0700

    Setting up fullstack nuxt app.

---
+++ components/layout/navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BackButton from "/components/elements/BackButton.vue"
+import {useState} from "#app"
+
+const user = useState('user')
+const setColorTheme = (newTheme: Theme) => {
+    useColorMode().preference = newTheme
+}
+
+</script>
+
+<template>
+
+
+
+</template>
